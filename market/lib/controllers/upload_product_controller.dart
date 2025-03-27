6a8cf05140c8f4b29ee78699e13c9d82a20777ea
alejo <aarangom1@eafit.edu.co>
2025-03-27 11:50:42 -0500

    Vista nueva de subida de productos y subida de productos exitosa mandando el companyId en el endpoint

--- market/lib/controllers/upload_product_controller.dart
+++ market/lib/controllers/upload_product_controller.dart
@@ -2,10 +2,27 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:market/models/product_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UploadProductController {
-  Future<void> uploadProduct(Product product) async {
-    const String apiUrl = 'http://10.0.2.2:8000/product'; // URL del backend
+  Future<void> uploadProduct(
+    Product product, [
+    String? companyId,
+  ]) async {
+    // Obtener el CompanyId de SharedPreferences si no se proporciona uno
+    if (companyId == null) {
+      final prefs = await SharedPreferences.getInstance();
+      companyId = prefs.getString('company_id');
+
+      if (companyId == null) {
+        throw Exception(
+          'No se encontró un CompanyId. Crea una empresa primero.',
+        );
+      }
+    }
+
+    final String apiUrl =
+        'http://10.0.2.2:8000/product/$companyId'; // URL del backend
 
     try {
       final response = await http.post(
@@ -14,10 +31,15 @@ class UploadProductController {
         body: jsonEncode(product.toJson()),
       );
 
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        print("Producto subido exitosamente: ${response.body}");
+      if (response.statusCode == 200 ||
+          response.statusCode == 201) {
+        print(
+          "Producto subido exitosamente: ${response.body}",
+        );
       } else {
-        throw Exception('Error al subir el producto: ${response.body}');
+        throw Exception(
+          'Error al subir el producto: ${response.body}',
+        );
       }
     } catch (e) {
       throw Exception('Error en la conexión: $e');
--- market/lib/views/business_screens/vendor_screen.dart
+++ market/lib/views/business_screens/vendor_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:market/views/business_screens/menu_navigation/upload_product_screen.dart';
+//import 'package:market/views/business_screens/menu_navigation/upload_product_screen.dart';
+import 'package:market/views/business_screens/menu_navigation/store_preview_screen.dart';
 //import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -199,7 +200,10 @@ class _VendorScreenState extends State<VendorScreen> {
                 context,
                 MaterialPageRoute(
                   builder:
-                      (context) => UploadProductScreen(),
+                      (context) => StorePreviewScreen(
+                        businessName: widget.businessName,
+                        businessLogo: widget.businessLogo,
+                      ),
                 ),
               );
             },
