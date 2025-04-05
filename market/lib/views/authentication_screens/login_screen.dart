9a764e7ff57abf4009219f15f92938843caa273d
alejo <aarangom1@eafit.edu.co>
2025-04-05 07:37:23 -0500

    se corrigen errores en las importaciones de varias vistas

--- market/lib/views/authentication_screens/login_screen.dart
+++ market/lib/views/authentication_screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:market/views/authentication_screens/forgot_password_screen.dart';
 import 'package:market/views/authentication_screens/register_screen.dart';
 import 'package:market/views/client_screen/user_screen.dart';
-import 'package:market/views/main_screen/businesses.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:market/views/widgets/remember_me_checkbox.dart';
